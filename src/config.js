9b4ab120811ebe9f5093bfbb7c73a0edb88c844a
Marwan361 <60656147+Marwan361@users.noreply.github.com>
2022-02-06 18:02:28 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 module.exports = {
-    token: process.env.TOKEN || "",  // your bot token
+    token: process.env.TOKEN || "OTIwMDgzNjA5NzQ5MTg4Njg4.YbfMog.ylbtcaX6JWsuwU_lsSnMmESiqyU",  // your bot token
     prefix: process.env.PREFIX || "!", // bot prefix
     ownerID: process.env.OWNERID || "491577179495333903", //your discord id
-    mongourl: process.env.MONGO_URI || "", // MongoDb URL
+    mongourl: process.env.MONGO_URI || "mongodb+srv://YassineHzz:YassineHzz@cluster0.vo589.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // MongoDb URL
     embedColor: process.env.COlOR || "#303236", // embed colour
     logs: process.env.LOGS || "875254787191504947", // channel id for guild create and delete logs
 
